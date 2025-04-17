a316e00ad312f3da9c288cc49bc57a26fb7fdc26
umutsiardemir <umutsiardemir@posta.mu.edu.tr>
2025-04-17 13:16:56 +0300

    week8 done

---
+++ Rectangle.java
@@ -0,0 +1,4 @@
+public class Rectangle extends Shape{
+    private int sideA:0,sideB:0;
+    this.sideA = sideA
+}
---
+++ TestShapes.java
@@ -0,0 +1,8 @@
+public class TestShapes {
+    public static void main(String[] args) {
+        Shape square = new Shape( "blue");
+        square.describe(5 ,4);
+    }
+
+
+}
